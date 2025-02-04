552be2c714bfdea5f0c282728123b18432a6dd8c
yoman321 <luophilipe@gmail.com>
2025-02-03 19:03:12 -0500

    started building main page

--- extension-react/src/App.tsx
+++ extension-react/src/App.tsx
@@ -8,6 +8,8 @@ import { VideoURL } from './common/types'
 import Logo from './components/Logo'
 
 import Grid from '@mui/material/Unstable_Grid2'
+import Box from '@mui/material/Box'
+import Divider from '@mui/material/Divider'
 
 const App = () => {
   const [currVideoURL, setCurrVideoURL] = useState("")
@@ -21,9 +23,25 @@ const App = () => {
 
   return (
     <Grid container spacing={2}>
-      <Grid xs={12}>
+      <Grid xs={8}>
         <Logo />
+      </Grid>
+      <Grid xs={4} display="flex" justifyContent="center" alignItems="center">
+        <Box display="flex" justifyContent="center" alignItems="center">Login/Register</Box>
+      </Grid>
+      <Grid xs={12}>
+        <Divider variant='middle' sx={{ backgroundColor: 'black', boxShadow: "0px 0px 10px gray" }} aria-hidden='true' />
+      </Grid>
+      <Grid xs={12}>
         <div>{currVideoURL}</div>
+        <div>{currVideoURL}</div>
+        <div>{currVideoURL}</div>
+        <div>{currVideoURL}</div>
+        <div>{currVideoURL}</div>
+        <div>{currVideoURL}</div>
+      </Grid>
+      <Grid xs={12}>
+        <Divider variant='middle' sx={{ backgroundColor: 'black', boxShadow: "0px 0px 10px gray" }} aria-hidden='true' />
       </Grid>
     </Grid>
   );
