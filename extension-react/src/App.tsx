73893c99d376664424c7ce004642485725336c32
yoman321 <luophilipe@gmail.com>
2025-02-03 21:35:50 -0500

    finished basic layout for main page -> to implement logic to load fact checked data

--- extension-react/src/App.tsx
+++ extension-react/src/App.tsx
@@ -10,6 +10,10 @@ import Logo from './components/Logo'
 import Grid from '@mui/material/Unstable_Grid2'
 import Box from '@mui/material/Box'
 import Divider from '@mui/material/Divider'
+import Link from '@mui/material/Link'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import FormGroup from '@mui/material/FormGroup'
+import Switch from '@mui/material/Switch'
 
 const App = () => {
   const [currVideoURL, setCurrVideoURL] = useState("")
@@ -30,21 +34,41 @@ const App = () => {
         <Box display="flex" justifyContent="center" alignItems="center">Login/Register</Box>
       </Grid>
       <Grid xs={12}>
-        <Divider variant='middle' sx={{ backgroundColor: 'black', boxShadow: "0px 0px 10px gray" }} aria-hidden='true' />
+        <Divider sx={{ backgroundColor: 'black', boxShadow: "0px 0px 10px gray" }} aria-hidden='true' />
+      </Grid>
+      <Grid container rowSpacing={1} xs={12}>
+        <Grid xs={12}>
+          <Box>{currVideoURL}</Box>
+        </Grid>
+        <Grid xs={12}>
+          <Box>{currVideoURL}</Box>
+        </Grid>
+        <Grid xs={12}>
+          <Box>{currVideoURL}</Box>
+        </Grid>
+        <Grid xs={12}>
+          <Box>{currVideoURL}</Box>
+        </Grid>
+        <Grid xs={12}>
+          <Box>{currVideoURL}</Box>
+        </Grid>
+        <Grid xs={12}>
+          <Box>{currVideoURL}</Box>
+        </Grid>
       </Grid>
       <Grid xs={12}>
-        <div>{currVideoURL}</div>
-        <div>{currVideoURL}</div>
-        <div>{currVideoURL}</div>
-        <div>{currVideoURL}</div>
-        <div>{currVideoURL}</div>
-        <div>{currVideoURL}</div>
+        <Divider sx={{ backgroundColor: 'black', boxShadow: "0px 0px 10px gray" }} aria-hidden='true' />
       </Grid>
-      <Grid xs={12}>
-        <Divider variant='middle' sx={{ backgroundColor: 'black', boxShadow: "0px 0px 10px gray" }} aria-hidden='true' />
+      <Grid xs={7}>
+        <Link href="https://github.com/stanley-utf8/healthy-ui">Learn more about this project</Link>
+      </Grid>
+      <Grid xs={5}>
+        <FormGroup>
+          <FormControlLabel labelPlacement="top" control={<Switch />} label='Fact-Check' />
+        </FormGroup>
       </Grid>
     </Grid>
-  );
+  )
 }
 
 export default App;
